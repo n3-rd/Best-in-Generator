74aeff568218654902966d91e23272f24702687b
n3-rd <godwinjdayo@gmail.com>
2022-06-30 23:11:45 +0100

    added readme and favicons

--- js/index.js
+++ js/index.js
@@ -1,32 +1,53 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let generateButton = document.querySelector("#generate");
+  var imageContainer = document.querySelector("#image");
 
-
-document.addEventListener('DOMContentLoaded', function() {
- 
-    let generateButton = document.querySelector('#generate');
-
-    generateButton.addEventListener('click', function() {
-        generateMeme()  
-
+  document
+    .querySelector("#image-caption")
+    .addEventListener("keyup", function (e) {
+      if (e.keyCode === 13) {
+        generateMeme();
+      }
     });
 
-let generateMeme = ()=> {
-    let caption = document.querySelector('#image-caption').value;
-    let imageContainer = document.querySelector('#image');
-    let downloadImage = document.querySelector('#download-image');
-    let loader = document.querySelector('.loader')
+  generateButton.addEventListener("click", function () {
+    generateMeme();
+  });
 
-    loader.style.display = 'block';
+  let downloadRawImage = document.querySelector("#download");
+  downloadRawImage.addEventListener("click", function () {
+    downloadImage();
+  });
 
-        fetch(`https://api.imgflip.com/caption_image?template_id=393371323&username=N3-rd&password=1232112321&text1=${caption}`)
-        .then(res => res.json())
-        .then(data => {
-            imageContainer.src = data.data.url;
-            downloadImage.href = data.data.url;
-            loader.style.display = 'none';
+  let downloadImage = () => {
+    fetch(imageContainer.src)
+      .then((resp) => resp.blob())
+      .then((blobobject) => {
+        const blob = window.URL.createObjectURL(blobobject);
+        const anchor = document.createElement("a");
+        anchor.style.display = "none";
+        anchor.href = blob;
+        anchor.download = `bestin${Date.now()}.png`;
+        document.body.appendChild(anchor);
+        anchor.click();
+        window.URL.revokeObjectURL(blob);
+      })
+      .catch(() => console.log("An error in downloadin gthe file sorry"));
+  };
 
-        })
-    }
-    
+  let generateMeme = () => {
+    let caption = document.querySelector("#image-caption").value;
+    let loader = document.querySelector(".loader");
 
+    loader.style.display = "block";
 
+    fetch(
+      `https://api.imgflip.com/caption_image?template_id=393371323&username=N3-rd&password=1232112321&text1=${caption}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        imageContainer.src = data.data.url;
+        loader.style.display = "none";
+      });
+  };
 });
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Best In Generator
+Hello comrades, 
+
+![Image](https://i.imgflip.com/6lf04u.jpg)
+
+## No need for further explanations then.
